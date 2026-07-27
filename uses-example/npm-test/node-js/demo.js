6d3948e2a2522bbac1a0f801122cdd7967d02e22
ranjeet <eejeet@gmail.com>
2026-07-28 00:49:38 +0530

    feat: restructure npm-test example folder into node-js and typescript subfolders

--- uses-example/npm-test/node-js/demo.js
+++ uses-example/npm-test/node-js/demo.js
@@ -1,13 +1,13 @@
 import devdoot from 'devdoot';
 
-console.log('=== Running Devdoot from NPM Registry ===');
+console.log('=== Running Devdoot JS from NPM Registry ===');
 
 // 1. Log simple messages
-devdoot.log('Application started successfully.');
+devdoot.log('JS App started successfully.');
 
 // 2. Create isolated group logger
 const dbLog = devdoot.group('Database');
-dbLog.log('Connected to remote PostgreSQL instance.');
+dbLog.log('Connected to JS DB Instance.');
 
 // 3. Info message
 devdoot.info('Process heartbeat active.');
